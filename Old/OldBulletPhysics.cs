e079cdff30b33fe4102a8b52bfa65c38c8766171
MichaelLawrenceDev <michael@mikedevelops.com>
2021-10-23 20:51:04 -0500

    Add files via upload

---
+++ Old/OldBulletPhysics.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class OldBulletPhysics : MonoBehaviour
+{
+    [Header("Bullet Properties")]
+    [SerializeField] ParticleSystem trail;
+    [SerializeField] int damage;
+    [SerializeField] float speed;
+    [SerializeField] float timeTillKill;
+    float gracePeriod = 0f; // Time until bullet becomes active
+    bool collisionActive = false;
+    bool isDestroyed = false;
+    float currentTime = 0;
+    GameObject bulletPivot;
+
+    public void Start()
+    {
+        // set pivot to center
+        bulletPivot = new GameObject("Bullet Pivot");
+        bulletPivot.transform.rotation = transform.rotation;
+        transform.parent = bulletPivot.transform;
+    }
+
+    public void Update()
+    {
+        // Move bullet
+        if (!isDestroyed)
+        {
+            bulletPivot.transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+
+            // Destroy if time elapsed
+            if (currentTime > timeTillKill)
+            {
+                StartCoroutine(DestroyBullet()); // Destroy in 2s
+            }
+
+            if (currentTime > gracePeriod) collisionActive = true;
+            currentTime += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (collisionActive && other.gameObject.layer == LayerMask.NameToLayer("player"))
+        {
+            TankController tc = other.gameObject.GetComponent<TankController>();
+            tc.dealDamage(damage);
+            StartCoroutine(DestroyBullet());
+        }
+    }
+    IEnumerator DestroyBullet()
+    {
+        // disable for 2s, let trail finish
+        isDestroyed = true;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<Collider>().enabled = false;
+        trail.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        yield return new WaitForSeconds(2f);
+
+        Destroy(bulletPivot);
+        Destroy(gameObject);
+    }
+}
---
+++ Old/RichochetDemo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RichochetDemo : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 startPos;
+    Quaternion startRot;
+    float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(new Vector3(0, 0, 1) * speed, ForceMode.VelocityChange);
+        startPos = transform.position;
+        startRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Reset
+            transform.position = startPos;
+            transform.rotation = startRot;
+            rb.velocity = Vector3.zero;
+            rb.AddRelativeForce(new Vector3(0, 0, 1) * 5f, ForceMode.VelocityChange);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with: " + collision.gameObject.name);
+        Vector3 normal = collision.GetContact(0).normal;
+        Vector3 reflect = Vector3.Reflect(rb.velocity, normal);
+
+        Debug.Log("normal: " + normal);
+        Debug.Log("reflect: " + reflect);
+        Debug.Log(Vector3.Angle(rb.velocity, normal));
+        // rb.velocity = reflect * speed;
+        Vector3 rot = transform.localEulerAngles;
+        rot.y += Vector3.Angle(rb.velocity, normal);
+        transform.localEulerAngles = rot;
+        rb.velocity = transform.forward * speed;
+    }
+}
