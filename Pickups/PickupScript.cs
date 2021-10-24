aa44a977c30efa8eb1a2abc936efcbbf236484ec
MichaelLawrenceDev <michael@mikedevelops.com>
2021-10-23 20:51:13 -0500

    Add files via upload

---
+++ Pickups/PickupScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{
+    [Header("Pickup Information")]
+    [SerializeField] int weaponType;
+    bool preventPickup = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("player") && !preventPickup)
+        {
+            TankController tc = other.GetComponent<TankController>();
+            if (tc.Pickup(weaponType))
+            {
+                preventPickup = true;
+                Destroy(transform.root.gameObject);
+            }
+                
+        }
+    }
+}
